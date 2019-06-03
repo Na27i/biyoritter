bc06721b517958581731a9bf53bc6f07273da876
Na27i <oanazzi.setter@gmail.com>
2019-06-04 03:20:05 +0900

    clearing code.

--- biyoritter.py
+++ biyoritter.py
@@ -3,10 +3,8 @@ import json
 import re
 import time
 import calendar
+import random
 from janome.tokenizer import Tokenizer
-from janome.analyzer import Analyzer
-from janome.charfilter import *
-from janome.tokenfilter import *
 from requests_oauthlib import OAuth1Session
 
 # 置換コマンド
@@ -14,9 +12,9 @@ import cmd
 
 # 認証情報
 args = sys.argv
-if len(args) == 1 :
+if len(args) == 1:
     import main as settings
-else :
+else:
     import sub as settings
 
 CK = settings.CONSUMER_KEY
@@ -26,9 +24,10 @@ ATS = settings.ACCESS_TOKEN_SECRET
 twitter = OAuth1Session(CK, CS, AT, ATS)
 
 post_url = "https://api.twitter.com/1.1/statuses/update.json"
-get_url  = "https://api.twitter.com/1.1/statuses/user_timeline.json"
-src_url  = "https://api.twitter.com/1.1/search/tweets.json"
+get_url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+src_url = "https://api.twitter.com/1.1/search/tweets.json"
 upload_url = "https://upload.twitter.com/1.1/media/upload.json"
+
 
 # 線を引くだけ
 def draw_line():
@@ -44,31 +43,35 @@ def time_cnv(created_at):
     time_local = time.localtime(unix_time)
     return time.strftime("%Y-%m-%d %H:%M:%S", time_local)
 
+
 # TL表示(検索)
 def mtl(count, sent):
     if sent == None:
         get_params = {"count": count}
-        get_res = twitter.get(get_url, params = get_params)
+        get_res = twitter.get(get_url, params=get_params)
     else:
         sent += " exclude:retweets exclude:replies"
         get_params = {"q": sent, "count": count}
-        get_res = twitter.get(src_url, params = get_params)
+        get_res = twitter.get(src_url, params=get_params)
 
     if get_res.status_code == 200:
         timelines = json.loads(get_res.text)
         draw_line()
         if sent == None:
             for get_tweet in timelines:
-                print("\n  " + get_tweet['user']['name'] + "   (@" + get_tweet['user']['screen_name'] + ")")
+                print("\n  " + get_tweet['user']['name'] + "   (@" +
+                      get_tweet['user']['screen_name'] + ")")
                 print(get_tweet['text'])
                 print("\n" + "[ " + time_cnv(get_tweet['created_at']) + " ]\n")
                 draw_line()
         else:
             for get_tweet in timelines["statuses"]:
-                print("\n  " + get_tweet['user']['name'] + "   (@" + get_tweet['user']['screen_name'] + ")")
+                print("\n  " + get_tweet['user']['name'] + "   (@" +
+                      get_tweet['user']['screen_name'] + ")")
                 print(get_tweet['text'])
                 print("\n" + "[ " + time_cnv(get_tweet['created_at']) + " ]\n")
                 draw_line()
+
 
 # コマンドを表示
 def cmd_disp():
@@ -80,6 +83,7 @@ def cmd_disp():
         print("   " + cmd.oth_cmd[i][0].ljust(4) + " : " + cmd.oth_cmd[i][1])
     print("")
 
+
 # ツイート本文の置換
 def rep(sent):
     for i in range(cmd.cmd_len):
@@ -87,36 +91,47 @@ def rep(sent):
             sent = sent.replace(cmd.rep_cmd[i][0], cmd.rep_cmd[i][1])
     return sent
 
+
 # ツイートの自動生成
 def gen():
+    tweet = ""
     tweetlist = ""
-    cnt = 0
-    get_params = {"count" : 1, "exclude_replies":True, "include_rts":False}
-    get_res = twitter.get(get_url, params = get_params)
+    do = []
+    get_url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+    get_params = {"count": 1, "exclude_replies": True, "include_rts": False}
+    get_res = twitter.get(get_url, params=get_params)
     if get_res.status_code == 200:
         timelines = json.loads(get_res.text)
-        for roop in timelines:
-            tweetlist = (roop["text"])
-        a = Analyzer(token_filters=[POSKeepFilter(['動詞'])])
-        for token in a.analyze(tweetlist):
-            cnt += 1
+        #全ツイートを取得
+        for i in timelines:
+            tweet = (i["text"])
+            tweet = tweet.split("http" , 1)[0]   #urlを削除
+            tweet = tweet.split("@", 1)[0]  #usernameを削除
+            tweet = tweet.split(" ")[0]   #半角スペースを削除
+            tweet = tweet.split("　")[0]   #全角スペースを削除
+            tweetlist += tweet
 
-        if cnt == 0:
-            post_params = {"status" : "ウチは何もしたくない気分なのん"}
-            post_res = twitter.post(post_url, params = post_params)
-        elif cnt == 1:
-            post_params = {"status" : "ウチも" + token.base_form + "のん！"}
-            post_res = twitter.post(post_url, params = post_params)
-        else:
-            post_params = {"status" : "ウチにはやることがいっぱいなのん……"}
-            post_res = twitter.post(post_url, params = post_params)
+        post_url = "https://api.twitter.com/1.1/statuses/update.json"
+        t = Tokenizer()
+        tokens = t.tokenize(tweetlist)
+        for token in tokens:
+            partOfSpeech = token.part_of_speech.split(',')[0]
+            if partOfSpeech == u"動詞":
+                do.append(token.base_form)
+
+        if len(do) == 0:
+            post_params = {"status": "ウチは何もしたくない気分なん"}
+            post_res = twitter.post(post_url, params=post_params)
+        else :
+            sent =  "ウチも" + do[random.randrange(len(do))] + "のん！"
+            post_params = {"status": sent}
+            post_res = twitter.post(post_url, params=post_params)
 
         if post_res.status_code != 200:
-            print("ウチは暇じゃないのん……")
-    else :
-        print("なにかがおかしいのん……")
+            print("二番煎じは面白くないのん……\n")
+    else:
+        print("ツイートの生成に失敗したのんな～\n")
     print("")
-
 
 
 while 1:
@@ -161,8 +176,8 @@ while 1:
             break
         elif re.match(img_reg, sent):
             try:
-                img = {"media" : open(sent[4:].replace('\n',''),'rb')}
-                img_obj = twitter.post(upload_url, files = img)
+                img = {"media": open(sent[4:].replace('\n', ''), 'rb')}
+                img_obj = twitter.post(upload_url, files=img)
                 img_id = json.loads(img_obj.text)['media_id']
                 break
             except FileNotFoundError:
@@ -183,16 +198,16 @@ while 1:
         else:
             tweet += rep(sent)
             tweet += "\n"
-    
+
     if flag == 1:
         tweet = ""
     elif flag != 1:
         if img_id:
-            post_params = {"status" : tweet, "media_ids":[img_id]}
+            post_params = {"status": tweet, "media_ids": [img_id]}
         else:
-            post_params = {"status" : tweet}
-        post_res = twitter.post(post_url, params = post_params)
-        
+            post_params = {"status": tweet}
+        post_res = twitter.post(post_url, params=post_params)
+
         if post_res.status_code != 200:
             print("投稿に失敗したのんな～\n")
         else:
