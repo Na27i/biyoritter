cee2384c36014ef2771d9cbdb2f94f4641ada7f0
Nazzi <32100067+Na27i@users.noreply.github.com>
2019-06-21 18:08:09 +0900

    remove sub_temp.py

--- sub_temp.py
+++
@@ -1,6 +0,0 @@
-# 主アカウントの認証情報設定ファイル(テンプレート)
-# 使うときに sub_temp.py を sub.py にリネームして使うのんな～
-CONSUMER_KEY = "XXXXXXXXXX"
-CONSUMER_SECRET = "XXXXXXXXXX"
-ACCESS_TOKEN = "XXXXXXXXXX"
-ACCESS_TOKEN_SECRET = "XXXXXXXXXX"
