d96906483219683c44e34b7006235d1848765631
Nazzi <32100067+Na27i@users.noreply.github.com>
2019-06-21 18:07:52 +0900

    remove main_temp.py

--- main_temp.py
+++
@@ -1,6 +0,0 @@
-# 主アカウントの認証情報設定ファイル(テンプレート)
-# 使うときに main_temp.py を main.py にリネームして使うのんな～
-CONSUMER_KEY = "XXXXXXXXXX"
-CONSUMER_SECRET = "XXXXXXXXXX"
-ACCESS_TOKEN = "XXXXXXXXXX"
-ACCESS_TOKEN_SECRET = "XXXXXXXXXX"
